ddf52450c7e866ff17b7554f79a82800a7226af3
ruuffian <ruuffian@gmail.com>
2025-02-05 12:47:58 -0500

    Added sorting to the teams column + trailing commas in prettierrc (this should have been done much earlier). Next commit will be a LOT of formatting changes. I'm also going to add the shadcn components to the prettier ignore file- those should never have been formatted by me anyways.

--- src/routes/players/+page.server.ts
+++ src/routes/players/+page.server.ts
@@ -4,28 +4,63 @@ export const load: PageServerLoad = async () => {
   const players: Player[] = [
     {
       name: 'ruuffian#FUNZ',
-      team: 'Naughty Bois',
-      active: true
+      team: 'zzNaughty Bois'
     },
     {
       name: 'Xellius#N B',
-      team: 'Naughty Bois',
-      active: true
+      team: 'zzNaughty Bois'
     },
     {
       name: 'Zebra#N B',
-      team: 'Naughty Bois',
-      active: true
+      team: 'zzNaughty Bois'
     },
     {
       name: 'Unkindness#N B',
-      team: 'Naughty Bois',
-      active: true
+      team: 'Naughty Bois'
     },
     {
       name: 'Seir#qiiqo',
-      team: 'Naughty Bois',
-      active: true
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'ruuffian#FUNZ',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Xellius#N B',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Zebra#N B',
+      team: 'aaNaughty Bois'
+    },
+    {
+      name: 'Unkindness#N B',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Seir#qiiqo',
+      team: 'aaNaughty Bois'
+    },
+    {
+      name: 'ruuffian#FUNZ',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Xellius#N B',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Zebra#N B',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Unkindness#N B',
+      team: 'Naughty Bois'
+    },
+    {
+      name: 'Seir#qiiqo',
+      team: 'Naughty Bois'
     }
   ];
   return {
--- src/routes/players/columns.ts
+++ src/routes/players/columns.ts
@@ -1,5 +1,6 @@
 import { renderComponent } from '$lib/components/ui/data-table';
 import DataTableActions from './data-table-actions.svelte';
+import DataTableTeamButton from './data-table-team-button.svelte';
 import type { ColumnDef } from '@tanstack/table-core';
 
 export type Player = {
@@ -10,16 +11,20 @@ export type Player = {
 export const columns: ColumnDef<Player>[] = [
   {
     accessorKey: 'name',
-    header: 'Name'
+    header: 'Name',
   },
   {
     accessorKey: 'team',
-    header: 'Team'
+    header: ({ column }) => {
+      return renderComponent(DataTableTeamButton, {
+        onclick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+      });
+    },
   },
   {
     id: 'actions',
     cell: ({ row }) => {
       return renderComponent(DataTableActions, { name: row.original.name });
-    }
-  }
+    },
+  },
 ];
