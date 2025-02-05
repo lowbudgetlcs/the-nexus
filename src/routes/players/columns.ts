282f7e115aa3336176b1411541f04da1a93fc250
ruuffian <ruuffian@gmail.com>
2025-02-04 20:55:19 -0500

    Removed active column bc what's even the point of it LOL. I need to go to sleep. Why am i coding the players page in the home-page branch? I lsot the plot, the yarn issues earlier tilted me too much.

--- src/routes/players/columns.ts
+++ src/routes/players/columns.ts
@@ -3,14 +3,9 @@ import type { ColumnDef } from '@tanstack/table-core';
 export type Player = {
   name: string;
   team: string;
-  active: boolean;
 };
 
 export const columns: ColumnDef<Player>[] = [
-  {
-    accessorKey: 'active',
-    header: 'Active'
-  },
   {
     accessorKey: 'name',
     header: 'Name'
