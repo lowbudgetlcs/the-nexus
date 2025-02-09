90cfade9df90d67e5de8a67fd94ab0b0bf90f7b7
ruuffian <ruuffian@gmail.com>
2025-02-09 03:21:32 -0500

    Fixed build failure and bumped version

--- src/lib/server/riot.ts
+++ src/lib/server/riot.ts
@@ -1,14 +1,17 @@
+export const prerender = false;
 import { RiotAPI, RiotAPITypes, PlatformId } from '@fightmegg/riot-api';
 import { env } from '$env/dynamic/private';
 import type { Result } from '$lib/types/result';
 
-const riot = new RiotAPI(env.RIOT_API_TOKEN);
+const rAPI = () => {
+  return new RiotAPI(env.RIOT_API_TOKEN);
+};
 export async function fetchAccountByRiotId(
   gameName: string,
   tagLine: string,
 ): Promise<Result<RiotAPITypes.Account.AccountDTO>> {
   try {
-    const account = await riot.account.getByRiotId({
+    const account = await rAPI().account.getByRiotId({
       region: PlatformId.AMERICAS,
       gameName,
       tagLine,
