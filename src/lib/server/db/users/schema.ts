db693bec97fd8493fdd35d423e8eb0dff3117e46
ruuffian <ruuffian@gmail.com>
2025-02-12 17:16:19 -0500

    Pulled latest database schema

--- src/lib/server/db/users/schema.ts
+++ src/lib/server/db/users/schema.ts
@@ -1,8 +1,9 @@
-import { sqliteTable, type AnySQLiteColumn, integer, text } from 'drizzle-orm/sqlite-core';
-import { sql } from 'drizzle-orm';
+import { sqliteTable, AnySQLiteColumn, integer, text } from "drizzle-orm/sqlite-core"
+  import { sql } from "drizzle-orm"
 
-export const users = sqliteTable('users', {
-  id: integer().primaryKey({ autoIncrement: true }),
-  username: text().notNull(),
-  password: text().notNull(),
+export const users = sqliteTable("users", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	username: text().notNull(),
+	password: text().notNull(),
 });
+
